7ad5efb07d490613b1c934f0bf31cf979138be17
VitalMangal <99019310+VitalMangal@users.noreply.github.com>
2022-10-20 13:40:24 +0500

    Delete scr directory

--- scr/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.row-menu__hidden-menu').click(function (event) {
-        $('.row-menu__hidden-menu,.row-menu__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
